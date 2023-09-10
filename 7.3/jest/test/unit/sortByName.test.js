bf18483cc71016c92faf2f3546de43a2bde69678
Igor Smirnov <inextgen@mail.ru>
2023-09-10 23:15:39 +0300

    100% coverage test added

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -14,4 +14,18 @@ describe("Books names test suit", () => {
       "Гарри Поттер",
     ]);
   });
+
+  it("Sort when all book names equal", () => {
+    expect(
+      sorting.sortByName([
+      "Гарри Поттер", 
+      "Гарри Поттер", 
+      "Гарри Поттер"
+    ])
+    ).toEqual([
+      "Гарри Поттер", 
+      "Гарри Поттер", 
+      "Гарри Поттер"
+    ]);
+  });
 });
